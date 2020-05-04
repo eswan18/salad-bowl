a563e723b2283194f08dded04d91c27c4207c56b
Ethan Swan <eswan18@rocketmail.com>
2020-05-03 23:32:47 -0400

    Use blueprints.

--- back-end/app.py
+++ back-end/app.py
@@ -1,5 +1,5 @@
 import redis
-from flask import Flask, request
+from flask import Flask, Blueprint
 from flask_restful import Api
 
 from resources import User, Game, Word
@@ -8,15 +8,14 @@ API_VERSION = '1'
 API_URL_PREFIX = f'/api/v{API_VERSION}'
 
 app = Flask(__name__)
-api = Api(app)
+api_bp = Blueprint('api', __name__, url_prefix=API_URL_PREFIX)
+api = Api(api_bp)
 
-api.add_resource(User,
-                 f'{API_URL_PREFIX}/user/<string:user_id>')
-api.add_resource(Game,
-                 f'{API_URL_PREFIX}/game',
-                 f'{API_URL_PREFIX}/game/<string:game_id>')
-api.add_resource(Word,
-                 f'{API_URL_PREFIX}/word')
+api.add_resource(User, '/user/<string:user_id>')
+api.add_resource(Game, '/game', '/game/<string:game_id>')
+api.add_resource(Word, '/word')
+
+app.register_blueprint(api_bp)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
