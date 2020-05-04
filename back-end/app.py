667bfc9b30eed19d4e78b9036daa6863be2bd195
Ethan Swan <eswan18@rocketmail.com>
2020-05-03 23:53:08 -0400

    Move all utilities to common/

--- back-end/app.py
+++ back-end/app.py
@@ -3,7 +3,6 @@ from flask import Flask, Blueprint
 from flask_restful import Api
 
 from resources import User, Game, Word
-from common.redis_connection import redis_connection as redis
 
 API_VERSION = '1'
 API_URL_PREFIX = f'/api/v{API_VERSION}'
@@ -12,12 +11,9 @@ app = Flask(__name__)
 api_bp = Blueprint('api', __name__, url_prefix=API_URL_PREFIX)
 api = Api(api_bp)
 
-api.add_resource(User, '/user/<string:user_id>',
-                 resource_class_kwargs={'redis': redis})
-api.add_resource(Game, '/game', '/game/<string:game_id>',
-                 resource_class_kwargs={'redis': redis})
-api.add_resource(Word, '/word',
-                 resource_class_kwargs={'redis': redis})
+api.add_resource(User, '/user/<string:user_id>')
+api.add_resource(Game, '/game', '/game/<string:game_id>')
+api.add_resource(Word, '/word')
 
 app.register_blueprint(api_bp)
 
--- back-end/resources/game.py
+++ back-end/resources/game.py
@@ -1,16 +1,14 @@
 from flask import request
 from flask_restful import Resource
 
-from .identifiers import random_id 
+from common.redis_connection import redis_connection as redis
+from common.identifiers import random_id
 
 class Game(Resource):
 
-    def __init__(self, redis):
-        self.redis = redis
-
     def get(self, game_id):
         '''Get the current round of a given game.'''
-        game_round = self.redis.get(f'game:{game_id}')
+        game_round = redis.get(f'game:{game_id}')
         if game_round is not None:
             return {'game_id': game_id, 'round': game_round}
         else:
@@ -21,11 +19,11 @@ class Game(Resource):
         game_id = random_id()
         # Make sure that ID isn't in the database -- repick until a new one is
         # found.
-        while self.redis.exists(f'game:{game_id}'):
+        while redis.exists(f'game:{game_id}'):
             game_id = random_id()
         # Merge the game_id (list of characters) into a single string.
         game_id = ''.join(game_id)
         # Update Redis with that game, and note it as being in round 0 (meaning
         # it hasn't begun).
-        self.redis.set(f'game:{game_id}', 0)
+        redis.set(f'game:{game_id}', 0)
         return {'game_id': game_id}
