9de84eaa7eb1bde9fef198ecbe2556547f8f3abc
Ethan Swan <eswan18@rocketmail.com>
2020-10-02 18:49:00 -0500

    Update start/stop Redis scripts.

--- back-end/start-redis.sh
+++ back-end/start-redis.sh
@@ -1,3 +1,3 @@
 REPO_LOCATION="/Users/eswan18/Develop/salad-bowl/back-end"
 
-docker run --name salad-bowl-redis -d -p 6379:6379 -v "$REPO_LOCATION/redis_data:/data" redis:latest redis-server --appendonly yes
+docker run --name salad-bowl-redis -d --rm -p 6379:6379 -v "$REPO_LOCATION/redis_data:/data" redis:latest redis-server --appendonly yes
